3f3479bf1b417790735aa6cfb8850eb73fe74a07
Isaac Halvorson <hello@hisaac.net>
2018-10-16 18:40:43 -0500

    Initial Commit

---
+++ PlainPaste/ViewController.swift
@@ -0,0 +1,21 @@
+// Created by Isaac Halvorson on 10/16/18
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
