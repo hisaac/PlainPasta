59121ada37eec88d2d8d4bde8ff5ceeb3c523012
Isaac Halvorson <hello@hisaac.net>
2018-10-16 20:15:07 -0500

    Stub out basic clipboard monitor method

--- PlainPaste/AppDelegate.swift
+++ PlainPaste/AppDelegate.swift
@@ -5,16 +5,37 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+	var timer: Timer?
+	let pasteboard = NSPasteboard.general
+	var counter = 0
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		startTimer()
 	}
 
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
+	func startTimer() {
+		timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+			self.checkPasteboard()
+		}
 	}
 
+	func checkPasteboard() {
+		if counter != pasteboard.changeCount {
+			counter = pasteboard.changeCount
 
+			if let string = pasteboard.string(forType: .string) {
+				print("string ===", string)
+			} else if let html = pasteboard.string(forType: .html) {
+				print("html ===", html)
+			} else if let rtf = pasteboard.string(forType: .rtf) {
+				print("rtf ===", rtf)
+			} else if let rtfd = pasteboard.string(forType: .rtfd) {
+				print("rtfd ===", rtfd)
+			} else if let tabularText = pasteboard.string(forType: .tabularText) {
+				print("tabularText ===", tabularText)
+			} else if let multipleTextSelection = pasteboard.string(forType: .multipleTextSelection) {
+				print("multipleTextSelection ===", multipleTextSelection)
+			}
+		}
+	}
 }
-
