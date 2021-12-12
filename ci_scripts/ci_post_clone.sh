1ff5085715930abfc277c70a317ae3b70ee41fdb
Isaac Halvorson <hello@hisaac.net>
2021-12-11 19:47:03 -0600

    Fix CI bootstrapping

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-project_root=$(dirname $CI_PROJECT_FILE_PATH)
-
-$project_root/.tuist-bin/tuist dependencies fetch
-$project_root/.tuist-bin/tuist generate
+cd "$(dirname "$CI_PROJECT_FILE_PATH")"
+.tuist-bin/tuist dependencies fetch
+.tuist-bin/tuist generate
 
 exit 0
